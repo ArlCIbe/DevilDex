2ee9c227c5e4d8437c0cdcf0d6c365a21feca2d6
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-12-17 18:45:55 -0600

    edited comments

--- backend/db_setup.py
+++ backend/db_setup.py
@@ -1,18 +1,19 @@
 import os #module that enables interaction w/ operating system
 from dotenv import load_dotenv #function that enables access to .env file content
-from sqlalchemy import create_engine #function that enables connection to database
-from sqlalchemy.orm import sessionmaker #function that enables interaction w/ database using sessions (aka Python objects)
+from sqlalchemy import create_engine #function that enables connection to db
+from sqlalchemy.orm import sessionmaker #function that enables interaction w/ db using sessions (aka Python objects)
 from sqlalchemy.ext.declarative import declarative_base #function that enables mapping of Python classes in code to tables in DD
 
 load_dotenv() #loads env variables from .env file
 
-DATABASE_URL = os.getenv("DATABASE_URL") #retrieves database URL from .env file
+DATABASE_URL = os.getenv("DATABASE_URL") #retrieves db URL from .env file
 
-engine = create_engine(DATABASE_URL) #establishes connection to DD database
+engine = create_engine(DATABASE_URL) #establishes connection to DD db
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False) #these parameters dictate the way the session interacts w/ the database
-#bind=engine; SessionLocal should only interact w/ databased defined by DATABASE_URL
+#sessionmaker() parameters dictate the way the session interacts w/ the db
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False) 
+#bind=engine; SessionLocal should only interact w/ db defined by DATABASE_URL
 #autocommit=False; must explicitly call session.commit() to save changes
-#autoflush=False; must explicity call session.flush() for code changes to reflect in database
+#autoflush=False; must explicity call session.flush() for code changes to reflect in db
 
-Base = declarative_base() #defines models that will correspond to tables in database
+Base = declarative_base() #defines models that will correspond to tables in db
