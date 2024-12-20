e725ee5259b492d450e192bf2bf2779a82338a6d
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-12-20 17:08:24 -0600

    corrected dotenv file path

--- backend/tests/test_dbconnection.py
+++ backend/tests/test_dbconnection.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError #exception object that indicates database operation issue
 from dotenv import load_dotenv 
 
-load_dotenv(dotenv_path=r'C:\Users\arlci\Documents\DEV\Portfolio\DevilDex\DevilDex\backend\.env')
+load_dotenv(dotenv_path=r'C:\Users\arlci\Documents\DEV\Portfolio\DevilDex\backend\.env')
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
