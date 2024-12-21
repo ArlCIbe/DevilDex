91ab6d2d6765922a05240fdbf459014a923c112c
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-12-20 23:55:02 -0600

    removed explicit path from load.dotenv()

--- backend/tests/test_dbconnection.py
+++ backend/tests/test_dbconnection.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import psycopg 
 
-load_dotenv(dotenv_path=r'C:\Users\arlci\Documents\DEV\Portfolio\DevilDex\backend\.env')
+load_dotenv() #loads the .env file into the script's environment
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -13,6 +13,6 @@ try:
         with conn.cursor() as cur: #creates cur object; used to execute SQL queries against the db
             cur.execute('SELECT 1 FROM devil_fruits') #returns the # 1
             result = cur.fetchall() #retrieves the 1st result from the query
-            print(result + 'Connected to Devil Fruit database successfully!')
+            print(f"Query result: {result} - Connected to Devil Fruit database successfully!")
 except Exception as e: #handles the exception
-    print('Unable to connect to Devil Fruit database: {e}')
+    print(f"Unable to connect to Devil Fruit database: {e}")
