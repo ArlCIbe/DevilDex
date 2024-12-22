967dd1835d71b224a4819f226a3e21730cd75aab
Arlinda C Ibezim <ArlCIbe@outlook.com>
2024-12-21 18:44:25 -0600

    changed formatting

--- backend/tests/test_dbconnection.py
+++ backend/tests/test_dbconnection.py
@@ -10,9 +10,14 @@ print(DATABASE_URL)
 
 try:
     with psycopg.connect(DATABASE_URL) as conn: #creates db connection; with ensures connection gets closed properly & conn object enables interaction w/ db
+
         with conn.cursor() as cur: #creates cur object; used to execute SQL queries against the db
+
             cur.execute('SELECT 1 FROM devilfruits') #returns the number 1
+
             result = cur.fetchall() #retrieves the 1st result from the query
+            
             print(f"Query result: {result} - Connected to Devil Fruit database successfully!")
+
 except Exception as e: #handles the exception
     print(f"Unable to connect to Devil Fruit database: {e}")
